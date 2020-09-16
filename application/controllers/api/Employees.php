c7e345759d83cd0fa27b39d3f2d7429231e087f4
sedera-tax <sedera.aina@gmail.com>
2020-09-16 17:10:07 +0400

    employee add with form validation

--- application/controllers/api/Employees.php
+++ application/controllers/api/Employees.php
@@ -5,11 +5,61 @@ use Restserver\Libraries\REST_Controller;
 
 class Employees extends REST_Controller
 {
+	public function __construct() {
+		parent::__construct();
+		$this->load->database();
+		// Load model
+		$this->load->model('Employees_model');
+	}
+
+	/**
+	 * @param null $id
+	 */
 	public function index_get($id = null) {
 		// TODO implement method
+		if ($id != NULL) {
+			$data = $this->db->get_where(
+				'employees',
+				['id' => $id]
+			)->row_array();
+
+			if ($data === NULL) {
+				return $this->response([
+					'status' => false,
+					'message' => 'No such employees with id = ' . $id . ' found'
+				], REST_Controller::HTTP_NOT_FOUND);
+			}
+		} else {
+			$data = $this->db->get('employees')->result();
+		}
+
+		return $this->response($data, REST_Controller::HTTP_OK);
 	}
 
 	public function index_post() {
 		// TODO implement method
+		$this->load->library('form_validation');
+		$this->form_validation->set_rules("first_name", "First Name", "required");
+		$this->form_validation->set_rules("last_name", "Last Name", "required");
+		$this->form_validation->set_rules("position_name", "Position Name", "required");
+		$this->form_validation->set_rules("email", "Email", "valid_email");
+		$this->form_validation->set_rules("telephone_number", "Telephone", "numeric");
+		if ($this->form_validation->run()) {
+			$input = $this->input->post();
+			$this->db->insert('employees', $input);
+
+			return $this->response(["message" => "OK", "rows_inserted" => 1], REST_Controller::HTTP_OK);
+		} else {
+			$res = [
+				'error'    => true,
+				'first_name_error' => form_error('first_name'),
+				'last_name_error' => form_error('last_name'),
+				'position_name_error' => form_error('position_name'),
+				'email_error' => form_error('email'),
+				'telephone_number_error' => form_error('telephone_number')
+			];
+
+			return $this->response($res, REST_Controller::HTTP_BAD_REQUEST);
+		}
 	}
 }
